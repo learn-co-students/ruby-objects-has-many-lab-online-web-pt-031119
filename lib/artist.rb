b44f90f740f2f98564e4092399e330d07ab7b17b
Kat B <katreesebarnes@gmail.com>
2019-04-12 22:35:47 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,34 @@
+require "pry"
 class Artist 
-  attr_accessor :name, :songs
+  attr_accessor :name
   
+  @@all = []
   def initialize(name)
     @name = name
-    @songs = []
+  @@all << self
   end
+  def self.all 
+    @@all
+  end
+ 
   def add_song(song)
-    @songs << song
+  
     song.artist = self
   end
  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+  def songs
+    Song.all.select do |s|
+      s.artist == self 
+    end
+
+    
+  end
+
+  def self.song_count
+    Song.all.count
+  end
 end
